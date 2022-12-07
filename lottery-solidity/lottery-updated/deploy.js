ee074bc909fc886b163478b718bb4699fa403507
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-12-07 20:48:15 +0400

    PickWinner configuration

---
+++ lottery-solidity/lottery-updated/deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+  "smile more near spawn weapon field soldier throw day wrong amused senior",
+  // remember to change this to your own phrase!
+  'https://goerli.infura.io/v3/53b00303e6804f0685f33e1ce7e30432'
+  // remember to change this to your own endpoint!
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
